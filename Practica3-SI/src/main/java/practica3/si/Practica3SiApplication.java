6090767b1a7fe65d8d68144990e1ffdbe1be55c3
saul alonso <saulete_1990@hotmail.com>
2017-04-19 10:29:28 +0200

    Inicio

---
+++ Practica3-SI/src/main/java/practica3/si/Practica3SiApplication.java
@@ -0,0 +1,12 @@
+package practica3.si;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica3SiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica3SiApplication.class, args);
+	}
+}
---
+++ Practica3-SI/src/test/java/practica3/si/Practica3SiApplicationTests.java
@@ -0,0 +1,16 @@
+package practica3.si;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Practica3SiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
