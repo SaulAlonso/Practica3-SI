46162aa090a899c10359d0720847e332a1f86e94
saul alonso <saulete_1990@hotmail.com>
2017-04-20 19:04:25 +0200

    BBDD funcionando, error al leer log

--- Practica3-SI/src/main/java/practica3/si/DataBaseLoader.java
+++ Practica3-SI/src/main/java/practica3/si/DataBaseLoader.java
@@ -27,6 +27,10 @@ public class DataBaseLoader {
 	@PostConstruct
 	public void initDatabase() {
 		String strLine = "";
+		String[] textoSplit;
+		String[] fechaSplit;
+		String[] textoRecurso;
+		String[] textoRecursoSplitEspacios;
 		try {
 			FileInputStream fstream = new FileInputStream("src/main/resources/static/mas-accesos-servidor-nitflex.log");
 			BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
@@ -35,15 +39,15 @@ public class DataBaseLoader {
 					System.out.println(strLine);
 					//Una vez extraida la linea actual del fichero log (strLine) utilizamos el metodo split para dividir la linea
 					//en diferentes String usando el simbolo "-" para dividirlos
-					String[] textoSplit = strLine.split("-");
+					textoSplit = strLine.split("\"-\"");
 					
 					//Buscamos el simbolo "+" que es el punto donde termina la fecha, siendo el caracter "[" el inicio de la fecha
 					//que al haber hecho split con "-" ya tenemos el valor de "[" que seria 0 o 1 entonces la fecha empieza en la
 					//posicion siguiente
-					int finFecha = strLine.indexOf("+");
-					
+					int finFecha = textoSplit[0].indexOf("+");
+					int inicioFecha = textoSplit[0].indexOf("[");
 					//Guardamos en fechaHora tanto la fecha como la hora, la fecha esta entre "/" y la hora entre ":"
-					String fechaHora = textoSplit[2].substring(2,finFecha-1);
+					String fechaHora = textoSplit[0].substring(inicioFecha+1,finFecha-1);
 					
 					//Como entre la fecha y la hora hay ":" cogemos el valor para tener un eje en el que movernos entre fecha y hora
 					int inicioHora = fechaHora.indexOf(":");
@@ -53,7 +57,7 @@ public class DataBaseLoader {
 					String horaString = fechaHora.substring(inicioHora+1,fechaHora.length());
 					
 					//De la variable fecha procedemos a extraer el dia, el mes y el año
-					String[] fechaSplit = fecha.split("/");
+					fechaSplit = fecha.split("/");
 					String diaString = fechaSplit[0];
 					String mesString = fechaSplit[1];
 					String anioString = fechaSplit[2];
@@ -63,15 +67,14 @@ public class DataBaseLoader {
 					
 					//Ahora procedemos con el recurso, siendo el inicio el simbolo " (comillas dobles) y el final el tamaño del String
 					//en la segunda posicion del array que anteriormente creamos al hacer split de la linea extraida del log
-					int inicioRecurso = strLine.indexOf("\"");
-					int finRecurso = strLine.indexOf(" ");
-					String textoRecurso = textoSplit[2].substring(inicioRecurso+1, finRecurso);
-					int finExtension = textoRecurso.lastIndexOf(".");
-					String recurso = textoRecurso.substring(finExtension+1,finRecurso);
+					textoRecurso = textoSplit[0].split("] \"");
+					textoRecursoSplitEspacios = textoRecurso[1].split(" ");		
+					int finExtension = textoRecursoSplitEspacios[1].lastIndexOf(".");
+					String recurso = textoRecursoSplitEspacios[1].substring(finExtension,textoRecursoSplitEspacios[1].length());
 					
 					//Cambiamos los tipos de datos de tiempo
 					int dia = Integer.parseInt(diaString);
-					int mes = Integer.parseInt(mesString);
+					int mes = 3;
 					int anio = Integer.parseInt(anioString);
 					int hora = Integer.parseInt(horaString);
 					
@@ -91,6 +94,5 @@ public class DataBaseLoader {
 		} catch (Exception e) {
 			System.err.println("Error: " + e.getMessage());
 		}
-
 	}
 }
